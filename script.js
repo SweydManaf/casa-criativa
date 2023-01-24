1847815630c431983427f3bf7604c8b1dbfda62e
Sweyd Manaf <abdulsweyd@gmail.com>
2023-01-25 01:15:29 +0200

    WorkshopDev Especial  - Episódio 3

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function onOff(){
-    document.querySelector('#modal').classList.toggle('hide') 
+    document.querySelector('#modal').classList.toggle('hide')
+    document.querySelector('body').classList.toggle('hideScroll')
+
+    document.querySelector('#modal').classList.toggle('addScroll')
 }
